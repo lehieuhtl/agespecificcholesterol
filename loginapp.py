653b2b5d6008b9c530731909d48f48d69d9d3dc0
Hieu Le <lehieu.htl@outlook.com>
2023-11-17 02:54:13 -0600

    loginapp.py update
    Got the login feature to redirect

--- loginapp.py
+++ loginapp.py
@@ -1,6 +1,8 @@
 import dash
 from dash import html, dcc, callback, Output, Input, State
+from dash.exceptions import PreventUpdate
 import pandas as pd 
+from dash import html
 
 #Load the cholesterol data
 agedata = pd.read_csv("agedata.csv")
@@ -8,8 +10,11 @@ agedata = pd.read_csv("agedata.csv")
 # Dash app with suppress_callback_exceptions=True
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
-#Dash app
-app = dash.Dash(__name__)
+# Load the content of the R Markdown file
+with open('dashboard.Rmd', 'r') as file:
+    rmarkdown_content = file.read()
+
+
 #Layout for login
 login_layout = html.Div([
   html.H2("Login System"),
@@ -30,14 +35,19 @@ dashboard_layout = html.Div([
     dcc.Graph(id='cholesterol-plot')
 ])
 
+# Layout for R Markdown content
+rmarkdown_layout = html.Div([
+    dcc.Markdown(id='rmarkdown-content', children=rmarkdown_content)
+])
+
+
 # App layout
 app.layout = html.Div([
-    # Initial page content is the login layout
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
-# Callback to switch between login page and dashboard based on login status
+# Callback to switch between pages based on URL pathname
 @app.callback(
     Output('page-content', 'children'),
     [Input('url', 'pathname')]
@@ -45,12 +55,18 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/dashboard':
         return dashboard_layout
+    elif pathname == '/login':
+        return login_layout
+    elif pathname == '/rmarkdown':
+        return rmarkdown_layout
     else:
+        # Redirect to default page (e.g., login)
         return login_layout
 
 # Callback to handle login button click
 @app.callback(
-    Output('login-status', 'children'),
+    [Output('login-status', 'children'),
+     Output('url', 'pathname')],
     [Input('login-button', 'n_clicks')],
     [State('username-input', 'value'),
      State('password-input', 'value')]
@@ -59,25 +75,12 @@ def handle_login(n_clicks, username, password):
     if n_clicks:
         # Simulate authentication 
         if username == 'demo' and password == 'password':
-            return 'Login successful! Redirecting to dashboard...'
+            return 'Login successful! Redirecting to dashboard...', '/dashboard'
         else:
-            return 'Invalid username or password. Please try again.'
+            return 'Invalid username or password. Please try again.', '/'
+    else:
+        # Initial state
+        raise PreventUpdate
 
-# Callback to update dashboard based on age group selection
-@app.callback(
-    Output('cholesterol-plot', 'figure'),
-    [Input('age-group-dropdown', 'value')]
-)
-def update_dashboard(age_group):
-    filtered_data = agedata[agedata['Age group'] == age_group]
-   
-    figure = {
-        'data': [{'x': filtered_data['Country'], 'y': filtered_data['Mean total cholesterol (mmol/L)'], 'type': 'bar', 'name': 'Cholesterol'}],
-        'layout': {'title': f'Cholesterol Levels for {age_group} Age Group', 'xaxis': {'title': 'Country'}, 'yaxis': {'title': 'Cholesterol Levels'}}
-    }
-    return figure
-
-# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-    
