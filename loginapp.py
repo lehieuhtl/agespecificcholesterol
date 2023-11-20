6a658456c0d1ec03e69865c3661e37d9d1d32ed1
Hieu Le <lehieu.htl@outlook.com>
2023-11-19 21:47:09 -0600

    update

--- loginapp.py
+++ loginapp.py
@@ -104,7 +104,6 @@
 #     app.run_server(debug=True)
 
 #REWRITE
-
 import dash
 from dash import html, dcc, callback, Output, Input, State
 from dash.exceptions import PreventUpdate
@@ -132,7 +131,7 @@ login_layout = html.Form([
 #Layout main page
 main_layout = html.Div([
     html.H2("Main Page"),
-    html.Button('View Dashboard', id='dashboard-button'),
+    html.Button('View Cholesterol Dashboard', id='dashboard-button'),
     html.Button('View R Markdown', id='rmarkdown-button'),
 ])
 
@@ -150,3 +149,60 @@ rmarkdown_layout = html.Div([
     dcc.Markdown(id='rmarkdown-content', children=rmarkdown_content)
 ])
 
+# App layout
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content', children=login_layout),
+    html.Div([
+        main_layout,
+        dashboard_layout,
+        rmarkdown_layout
+    ], style={'display': 'none'})
+])
+
+# Callback to switch between pages based on URL pathname
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')]
+)
+def display_page(pathname):
+    if pathname == '/dashboard':
+        return dashboard_layout
+    elif pathname == '/login':
+        return login_layout
+    elif pathname == '/rmarkdown':
+        return rmarkdown_layout
+    elif pathname == '/main':
+        return main_layout
+    else:
+        # Redirect to default page (e.g., login)
+        return login_layout
+
+# Callback to handle login, dashboard and rmarkdown button clicks
+@app.callback(
+    Output('url', 'pathname'),
+    [Input('login-button', 'n_clicks'), Input('dashboard-button', 'n_clicks'), Input('rmarkdown-button', 'n_clicks')],
+    [State('username-input', 'value'), State('password-input', 'value')],
+    prevent_initial_call=True
+)
+def update_url_pathname(login_clicks, dashboard_clicks, rmarkdown_clicks, username, password):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        raise PreventUpdate
+
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if button_id == 'login-button':
+        if username == 'demo' and password == 'password':
+            return '/main'
+        else:
+            return '/'
+    elif button_id == 'dashboard-button':
+        return '/dashboard'
+    elif button_id == 'rmarkdown-button':
+        return '/rmarkdown'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
